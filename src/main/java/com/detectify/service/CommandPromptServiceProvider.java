ac498eb0aa18216ac27fa22256807ccad44b7af4
Moupiya Ghosh <moupiya.pink@gmail.com>
2017-10-17 01:00:41 +0530

    Adding command prompt service provider

---
+++ src/main/java/com/detectify/service/CommandPromptServiceProvider.java
@@ -0,0 +1,65 @@
+package com.detectify.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Moupiya
+ *
+ */
+public class CommandPromptServiceProvider {
+	/**
+	 * @param args
+	 * @throws ParseException 
+	 */
+	public static void main(String[] args) throws ParseException {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ScreenShotService screenShotService = (ScreenShotService)context.getBean("screenShotService");
+		try
+		{
+			if ("takeScreenShotByUrl".equalsIgnoreCase(args[0]))
+			{
+				printFileName(screenShotService.takeScreenShots(args[1]));
+			}
+			else if("takeScreenShotByFile".equalsIgnoreCase(args[0]))
+			{
+				printFileName(screenShotService.takeScreenShots(new File(args[1])));
+			}
+			else if ("searchScreenShotByUrl".equalsIgnoreCase(args[0]))
+			{
+				printFileName(screenShotService.searchScreenShotsByUrl(args[1]));
+			}
+			else if ("searchScreenShotByDate".equalsIgnoreCase(args[0]))
+			{
+				printFileName(screenShotService.searchScreenShotsByDate(args[1], args[2]));
+			}
+			else if ("searchScreenShotByUrlAndDate".equalsIgnoreCase(args[0]))
+			{
+				printFileName(screenShotService.searchScreenShotsByUrlAndDate(args[1], args[2], args[3]));
+			}
+			else if ("searchScreenShotsByDateRange".equalsIgnoreCase(args[0]))
+			{
+				printFileName(screenShotService.searchScreenShotsByDateRange(args[1], args[2], args[3]));
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printFileName(List<File> files)
+	{
+		System.out.println("File names : ");
+		for(File file : files)
+		{
+			System.out.println(file.getAbsolutePath());
+		}
+	}
+
+
+}
