ca5a89c61c1d80a9ad9c647139a3c2ddf373dd37
KameliaGhosh <kameliaghosh@gmail.com>
2017-10-16 22:12:46 -0700

    Removing MessageService

--- src/main/java/com/detectify/controllers/ScreenShotController.java
+++ src/main/java/com/detectify/controllers/ScreenShotController.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,9 +18,6 @@ import javax.ws.rs.core.Response;
 import com.detectify.service.RequestProducer;
 import org.springframework.util.StringUtils;
 
-import com.detectify.service.MessageService;
-import com.detectify.service.ScreenShotService;
-import com.detectify.util.ContextProvider;
 import com.detectify.util.ScreenShotUtility;
 
 /**
